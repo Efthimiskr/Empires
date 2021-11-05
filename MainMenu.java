b9b942a457d4727ed0aeb00a2fb99e5d3e96a344
ALEXANDROS PAPPAS <80106020+AlexandrosAueb@users.noreply.github.com>
2021-11-05 21:37:26 +0200

    Upgraded GUI
    added farao

--- MainMenu.java
+++ MainMenu.java
@@ -3,7 +3,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Button;
 import java.awt.Color;
@@ -12,6 +15,7 @@ import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 import javax.swing.JFormattedTextField;
 import javax.swing.JTextPane;
+import java.awt.SystemColor;
 
 public class MainMenu {
 
@@ -49,15 +53,17 @@ public class MainMenu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(222, 184, 135));
+		frame.setBackground(Color.LIGHT_GRAY);
 		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 25));
 		frame.setBounds(100, 100, 1286, 769);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("RISK 2");
+		JLabel lblNewLabel = new JLabel("Last Empire");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 50));
-		lblNewLabel.setBounds(415, 10, 463, 94);
+		lblNewLabel.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 50));
+		lblNewLabel.setBounds(418, 10, 463, 94);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JButton btnNewButton = new JButton("START");
@@ -72,6 +78,7 @@ public class MainMenu {
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		textField.setBounds(185, 421, 193, 44);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
@@ -83,6 +90,7 @@ public class MainMenu {
 		frame.getContentPane().add(lblNewLabel_1_1);
 		
 		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		textField_1.setColumns(10);
 		textField_1.setBounds(185, 478, 193, 44);
 		frame.getContentPane().add(textField_1);
@@ -94,6 +102,7 @@ public class MainMenu {
 		frame.getContentPane().add(lblNewLabel_1_2);
 		
 		textField_2 = new JTextField();
+		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		textField_2.setColumns(10);
 		textField_2.setBounds(185, 536, 193, 44);
 		frame.getContentPane().add(textField_2);
@@ -105,16 +114,23 @@ public class MainMenu {
 		frame.getContentPane().add(lblNewLabel_1_3);
 		
 		textField_3 = new JTextField();
+		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		textField_3.setColumns(10);
 		textField_3.setBounds(185, 593, 193, 44);
 		frame.getContentPane().add(textField_3);
 		
 		JTextPane txtpnWelcomeTo = new JTextPane();
 		txtpnWelcomeTo.setEditable(false);
-		txtpnWelcomeTo.setBackground(Color.ORANGE);
-		txtpnWelcomeTo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		txtpnWelcomeTo.setText("Welcome to 34563 B.C.\r\n\r\ntext text text text text text text text \r\ntext text text text text text text text text text text text \r\ntext text text text text ");
+		txtpnWelcomeTo.setBackground(new Color(222, 184, 135));
+		txtpnWelcomeTo.setFont(new Font("Harrington", Font.PLAIN, 25));
+		txtpnWelcomeTo.setText("Welcome to 322 B.C.\r\n\r\nThe king of Macedonia Alexander led the Greeks in a successful campaign to overthrow the Persian & Egypt Empires. After Alexander's death in 323, his successors divided his empire into several kingdoms. Greek culture became widespread in the territories of these kingdoms and, after their conquest by Rome, in many parts of the Roman Empire . Do you believe you are capable of leading your army and conquer the other Empires?\r\n~Good Luck, Have Fun~\r\n");
 		txtpnWelcomeTo.setBounds(85, 126, 1101, 247);
 		frame.getContentPane().add(txtpnWelcomeTo);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		Image img=new ImageIcon(this.getClass().getResource("/Alexander.png")).getImage();
+		lblNewLabel_2.setIcon(new ImageIcon(img));
+		lblNewLabel_2.setBounds(399, 10, 129, 119);
+		frame.getContentPane().add(lblNewLabel_2);
 	}
 }
