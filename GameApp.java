3f679acb0789dd54ad5d6531405a04a1194cd6aa
Agapitos Oikonomidis <t8200127@aueb.gr>
2021-11-07 12:53:28 +0200

    Update GameApp.java

--- GameApp.java
+++ GameApp.java
@@ -2,6 +2,31 @@ public class GameApp {
 
 	public static void main(String[] args) {
 		
+		Region r1 = new Region("", 3, "Blue");
+		Region r2 = new Region("", 5, "Red");
+		Region r3 = new Region("", 3, "Green");
+		Region r4 = new Region("", 4, "Yellow");
+		Region r5 = new Region("", 4, "Blue");
+		Region r6 = new Region("", 6, "Red");
+		Region r7 = new Region("", 3, "Green");
+		Region r8 = new Region("", 2, "Yellow");
+		Region r9 = new Region("", 4, "Blue");
+		Region r10 = new Region("", 2, "Red");
+		Region r11 = new Region("", 4, "Green");
+		Region r12 = new Region("", 7, "Yellow");
+		Region r13 = new Region("", 7, "Blue");
+		Region r14 = new Region("", 4, "Red");
+		Region r15 = new Region("", 3, "Green");
+		Region r16 = new Region("", 5, "Yellow");
+		Region r17 = new Region("", 2, "Blue");
+		Region r18 = new Region("", 3, "Red");
+		Region r19 = new Region("", 7, "Green");
+		Region r20 = new Region("", 2, "Yellow");
+	
+		Player p1 = new Player("", "Blue", 20, 5);
+		Player p2 = new Player("", "Red", 20, 5);
+		Player p3 = new Player("", "Green", 20, 5);
+		Player p4 = new Player("", "Yellow", 20, 5);
 
 	}
 
