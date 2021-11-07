97b436a7235320b6b3c49f0ffe49e1d492266474
Agapitos Oikonomidis <t8200127@aueb.gr>
2021-11-07 12:52:03 +0200

    Update player.java
    Για να έχουμε κοινά ονόματα μεταβλητών

---
+++ Player.java
@@ -0,0 +1,40 @@
+
+public class Player {
+	
+	private String playerName;
+	private String playerColor;
+	private int playerSoldiers;
+	private int playerRegions;
+	public Player(String playerName, String playerColor, int playerSoldiers, int playerRegions) {
+		
+		this.playerName = playerName;
+		this.playerColor = playerColor;
+		this.playerSoldiers = playerSoldiers;
+		this.playerRegions = playerRegions;
+	}
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public String getPlayerColor() {
+		return playerColor;
+	}
+	public void setPlayerColor(String playerColor) {
+		this.playerColor = playerColor;
+	}
+	public int getPlayerSoldiers() {
+		return playerSoldiers;
+	}
+	public void setPlayerSoldiers(int playerSoldiers) {
+		this.playerSoldiers = playerSoldiers;
+	}
+	public int getPlayerRegions() {
+		return playerRegions;
+	}
+	public void setPlayerRegions(int playerRegions) {
+		this.playerRegions = playerRegions;
+	}
+}
+
