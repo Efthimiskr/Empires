5072261d58d3d4a5a19bcd79946037a8a7d9a07c
Agapitos Oikonomidis <t8200127@aueb.gr>
2021-11-16 11:33:45 +0200

    Update Function.java

--- Function.java
+++ Function.java
@@ -2,26 +2,26 @@ public class Function {
 	
 	public static void positioning() { // Creation of objects ( 20 Regions and 4 Players ) //
 		
-		Region r1 = new Region("", 3, "Blue");
-		Region r2 = new Region("", 5, "Red");
-		Region r3 = new Region("", 3, "Green");
-		Region r4 = new Region("", 4, "Yellow");
-		Region r5 = new Region("", 4, "Blue");
-		Region r6 = new Region("", 6, "Red");
-		Region r7 = new Region("", 3, "Green");
-		Region r8 = new Region("", 2, "Yellow");
-		Region r9 = new Region("", 4, "Blue");
-		Region r10 = new Region("", 2, "Red");
-		Region r11 = new Region("", 4, "Green");
-		Region r12 = new Region("", 7, "Yellow");
-		Region r13 = new Region("", 7, "Blue");
-		Region r14 = new Region("", 4, "Red");
-		Region r15 = new Region("", 3, "Green");
-		Region r16 = new Region("", 5, "Yellow");
-		Region r17 = new Region("", 2, "Blue");
-		Region r18 = new Region("", 3, "Red");
-		Region r19 = new Region("", 7, "Green");
-		Region r20 = new Region("", 2, "Yellow");
+		Region r1 = new Region("Athens", 3, "Blue");
+		Region r2 = new Region("Sparta", 5, "Red");
+		Region r3 = new Region("Korinth", 3, "Green");
+		Region r4 = new Region("Crete", 4, "Yellow");
+		Region r5 = new Region("Macedonia", 4, "Blue");
+		Region r6 = new Region("Rome", 6, "Red");
+		Region r7 = new Region("Ravenna", 3, "Green");
+		Region r8 = new Region("Sicily", 2, "Yellow");
+		Region r9 = new Region("Florence", 4, "Blue");
+		Region r10 = new Region("Sardinia", 2, "Red");
+		Region r11 = new Region("Thinis", 4, "Green");
+		Region r12 = new Region("Tanis", 7, "Yellow");
+		Region r13 = new Region("Alexandria", 7, "Blue");
+		Region r14 = new Region("Avaris", 4, "Red");
+		Region r15 = new Region("Memphis", 3, "Green");
+		Region r16 = new Region("Nineveh", 5, "Yellow");
+		Region r17 = new Region("Susa", 2, "Blue");
+		Region r18 = new Region("Ashur", 3, "Red");
+		Region r19 = new Region("Persepolis", 7, "Green");
+		Region r20 = new Region("Babylon", 2, "Yellow");
 	
 		Player p1 = new Player("", "Blue", 20, 5);
 		Player p2 = new Player("", "Red", 20, 5);
