fe415cbea95bc22ddf9cd54d57e081b28c6815e5
Agapitos Oikonomidis <t8200127@aueb.gr>
2021-11-20 14:47:12 +0200

    Update Region.java

--- Region.java
+++ Region.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
 public class Region {
 	
 	private String regionName;
 	private int regionSoldiers;
 	private String regionColor;
+	private ArrayList<String> borders = new ArrayList<String>();
 	
-	public Region(String regionName, int regionSoldiers, String regionColor) {
+	
+	public Region(String regionName, int regionSoldiers, String regionColor, ArrayList<String> borders) {
 		this.regionName = regionName;
 		this.regionSoldiers = regionSoldiers;
 		this.regionColor = regionColor;
+		this.borders = borders;
 	}
 
 	public String getRegionName() {
@@ -32,5 +36,15 @@ public class Region {
 
 	public void setRegionColor(String regionColor) {
 		this.regionColor = regionColor;
+	}
+
+	public ArrayList<String> getBorders() {
+		return borders;
+	}
+
+	public void setBorders(ArrayList<String> borders) {
+		this.borders = borders;
 	}	
+	
+	
 }
