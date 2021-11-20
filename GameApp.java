82377e1950e41f9ad37a8958e114fe25342a3c30
Agapitos Oikonomidis <t8200127@aueb.gr>
2021-11-20 15:40:02 +0200

    Update GameApp.java

--- GameApp.java
+++ GameApp.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-public class GameApp{
+public class GameApp {
 	
 	static ArrayList<String> br1 = new ArrayList<String>();
 	static ArrayList<String> br2 = new ArrayList<String>();
@@ -26,7 +26,7 @@ public class GameApp{
 	
 	static Region r1 = new Region("Athens", 3, "Blue", br1);
 	static Region r2 = new Region("Sparta", 5, "Red", br2);
-	static Region r3 = new Region("Korinth", 3, "Green", br3);
+	static Region r3 = new Region("Corinth", 3, "Green", br3);
 	static Region r4 = new Region("Crete", 4, "Yellow", br4);
 	static Region r5 = new Region("Macedonia", 4, "Blue", br5);
 	static Region r6 = new Region("Rome", 6, "Red", br6);
@@ -57,8 +57,85 @@ public class GameApp{
 	
 	static Player[] tablep = { p1, p2, p3, p4 };
 	
+	public static void borderPlacing() {
+		br1.add("Tanis");
+		br1.add("Macedonia");
+		br1.add("Sparta");
+		br2.add("Athens");
+		br2.add("Macedonia");
+		br2.add("Nineveh");
+		br2.add("Babylon");
+		br3.add("Ravenna");
+		br3.add("Macedonia");
+		br3.add("Susa");
+		br3.add("Alexandria");
+		br4.add("Nineveh");
+		br4.add("Ashur");
+		br4.add("Macedonia");
+		br4.add("Rome");
+		br4.add("Florence");
+		br5.add("Athens");
+		br5.add("Tanis");
+		br5.add("Ravenna");
+		br5.add("Corinth");
+		br5.add("Alexandria");
+		br5.add("Rome");
+		br5.add("Crete");
+		br5.add("Nineveh");
+		br5.add("Sparta");
+		br6.add("Alexandria");
+		br6.add("Macedonia");
+		br6.add("Crete");
+		br6.add("Florence");
+		br6.add("Sicily");
+		br7.add("Tanis");
+		br7.add("Macedonia");
+		br7.add("Corinth");
+		br7.add("Susa");
+		br8.add("Rome");
+		br8.add("Florence");
+		br9.add("Sicily");
+		br9.add("Rome");
+		br9.add("Crete");
+		br9.add("Ashur");
+		br9.add("Avaris");
+		br10.add("Avaris");
+		br10.add("Memphis");
+		br11.add("Persepolis");
+		br11.add("Susa");
+		br11.add("Alexadria");
+		br12.add("Athens");
+		br12.add("Macedonia");
+		br12.add("Ravenna");
+		br13.add("Thinis");
+		br13.add("Susa");
+		br13.add("Corinth");
+		br13.add("Macedonia");
+		br13.add("Rome");
+		br14.add("Florence");
+		br14.add("Sardinia");
+		br14.add("Memphis");
+		br15.add("Avaris");
+		br15.add("Sardinia");
+		br16.add("Sparta");
+		br16.add("Macedonia");
+		br16.add("Babylon");
+		br16.add("Crete");
+		br17.add("Ravenna");
+		br17.add("Corinth");
+		br17.add("Persepolis");
+		br17.add("Thinis");
+		br18.add("Crete");	
+		br18.add("Florence");
+		br19.add("Susa");
+		br19.add("Thinis");
+		br20.add("Sparta");
+		br20.add("Nineveh");
+	}
+	
 	public static void main(String[] args) {
-				
+		
+		Game.borderPlacing();
 		Function.gamestart();
 	}	
 }
