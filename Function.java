beaf582757a835e1ad1dd6f17a5f6ca0a16765cc
Agapitos Oikonomidis <t8200127@aueb.gr>
2021-11-20 14:48:13 +0200

    Update Function.java

--- Function.java
+++ Function.java
@@ -1,37 +1,42 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Function {
 	
-	public static void positioning() { // Creation of objects ( 20 Regions and 4 Players ) //
+	static Scanner keyboard = new Scanner(System.in);
 		
-		Region r1 = new Region("Athens", 3, "Blue");
-		Region r2 = new Region("Sparta", 5, "Red");
-		Region r3 = new Region("Korinth", 3, "Green");
-		Region r4 = new Region("Crete", 4, "Yellow");
-		Region r5 = new Region("Macedonia", 4, "Blue");
-		Region r6 = new Region("Rome", 6, "Red");
-		Region r7 = new Region("Ravenna", 3, "Green");
-		Region r8 = new Region("Sicily", 2, "Yellow");
-		Region r9 = new Region("Florence", 4, "Blue");
-		Region r10 = new Region("Sardinia", 2, "Red");
-		Region r11 = new Region("Thinis", 4, "Green");
-		Region r12 = new Region("Tanis", 7, "Yellow");
-		Region r13 = new Region("Alexandria", 7, "Blue");
-		Region r14 = new Region("Avaris", 4, "Red");
-		Region r15 = new Region("Memphis", 3, "Green");
-		Region r16 = new Region("Nineveh", 5, "Yellow");
-		Region r17 = new Region("Susa", 2, "Blue");
-		Region r18 = new Region("Ashur", 3, "Red");
-		Region r19 = new Region("Persepolis", 7, "Green");
-		Region r20 = new Region("Babylon", 2, "Yellow");
-	
-		Player p1 = new Player("", "Blue", 20, 5);
-		Player p2 = new Player("", "Red", 20, 5);
-		Player p3 = new Player("", "Green", 20, 5);
-		Player p4 = new Player("", "Yellow", 20, 5);
-	}
-	
 	public static void gamestart() { // Starts the game with round 1 //
-		for(int i = 0;i <= 50; i++) {
-			
+		for(int i = 1;i <= 50; i++) { 
+			for(int j = 0; j <= 3; j++) {
+				System.out.println(Game.tablep[j].getPlayerName()+" is your turn to play"); 
+				
+				
+				
+				
+				
+				
+				
+				System.out.println("From where do you want to attack ?"); // Attack
+				String tempColor = Game.tablep[j].getPlayerColor(); // Attacker's color ( temporary )
+				ArrayList<String> alliedStates = new ArrayList<String>();
+				for(int k = 0; k <= 19; k++) {
+					if(tempColor.equals(Game.tabler[k].getRegionColor())) {
+						alliedStates.add(Game.tabler[k].getRegionName());
+						System.out.println(Game.tabler[k].getRegionName());						
+					}
+				}
+				
+				boolean flag = true; // Check valid input
+				while(flag) {
+					String ra = keyboard.nextLine();
+					for(int counter = 0; counter <= alliedStates.size(); counter++) {
+						if(ra.equals(alliedStates.get(counter))) {
+							flag = false;
+							break;
+						}
+					}
+								
+				}
+			}
 		}
 	}
 	
@@ -43,8 +48,9 @@ public class Function {
 		
 	}
 	
-	public void attack() { // The option of attack ( from where to where; ) //
-	
+	public void attack(Region ra, Region rd) { // The option of attack ( from where to where; ) //
+		
+		
 	}
 	
 	public void fortify() { // The option of fortifying soldiers ( from where; to where; how much; ) //
